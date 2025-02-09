43348fcdc9cd981575daa1e9053bae7680f3ceb2
Andrew Paolini <andrewpaolini123@gmail.com>
2025-02-09 16:18:56 -0600

    vercel-production build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from 'next';
 
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
-const nextConfig: NextConfig = {
-	output: 'export', // Enables static export
-	basePath: isGithubPages ? '/paolini-sounds' : '', // Adjust basePath for GitHub Pages
-	assetPrefix: isGithubPages ? '/paolini-sounds/' : '', // Adjust assetPrefix for GitHub Pages
-	images: {
-		unoptimized: true, // Disables the Image Optimization API
-	},
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
